894a7f5b8d0053ed1880fa9c8f6992ab48318996
Jan-Klaas Kollhof <keyjaque@jsolait.net>
2006-04-04 20:20:31 +0000

    updated test to conform with new date encoding

--- test/test_xmlrpc.js
+++ test/test_xmlrpc.js
@@ -8,9 +8,9 @@ Module("test_xmlrpc", "0.0.1", function(mod){
         
         var o = [{a:"Hello ' \" World < & >", b:[1,2,3]}, true, false, new Date(0)];
         
-        testing.assertEquals(xmlrpc.marshall(o), "<array><data><value><struct><member><name>a</name><value><string>Hello ' \" World &lt; &amp; ></string></value></member><member><name>b</name><value><array><data><value><int>1</int></value><value><int>2</int></value><value><int>3</int></value></data></array></value></member></struct></value><value><boolean>1</boolean></value><value><boolean>0</boolean></value><value><dateTime.iso8601>19700101T00:00:00</dateTime.iso8601></value></data></array>");
+        testing.assertEquals(xmlrpc.marshall(o), "<array><data><value><struct><member><name>a</name><value><string>Hello ' \" World &lt; &amp; ></string></value></member><member><name>b</name><value><array><data><value><int>1</int></value><value><int>2</int></value><value><int>3</int></value></data></array></value></member></struct></value><value><boolean>1</boolean></value><value><boolean>0</boolean></value><value><dateTime.iso8601>19700101T00:00:00:000</dateTime.iso8601></value></data></array>");
         var r = s.echo(o);
-        testing.assertEquals(xmlrpc.marshall(r), "<array><data><value><struct><member><name>a</name><value><string>Hello ' \" World &lt; &amp; ></string></value></member><member><name>b</name><value><array><data><value><int>1</int></value><value><int>2</int></value><value><int>3</int></value></data></array></value></member></struct></value><value><boolean>1</boolean></value><value><boolean>0</boolean></value><value><dateTime.iso8601>19700101T00:00:00</dateTime.iso8601></value></data></array>");
+        testing.assertEquals(xmlrpc.marshall(r), "<array><data><value><struct><member><name>a</name><value><string>Hello ' \" World &lt; &amp; ></string></value></member><member><name>b</name><value><array><data><value><int>1</int></value><value><int>2</int></value><value><int>3</int></value></data></array></value></member></struct></value><value><boolean>1</boolean></value><value><boolean>0</boolean></value><value><dateTime.iso8601>19700101T00:00:00:000</dateTime.iso8601></value></data></array>");
     };
     
     mod.__main__=function(){
